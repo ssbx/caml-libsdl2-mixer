4ce30fe7d9e3f10043b05f8e0ad32f5836c3d4de
seb <seb@seb.seb>
2024-07-18 13:08:29 +0200

    format

--- mix.ml
+++ mix.ml
@@ -7,8 +7,8 @@ module InitFlags = struct
     | `MIX_INIT_MP3
     | `MIX_INIT_OGG
     ]
-  let test () =
-    print_endline "hello"
+
+  let test () = print_endline "hello"
 end
 
 module Default = struct
@@ -17,19 +17,19 @@ module Default = struct
   let chunk_size = 2048
 end
 
-external init : InitFlags.t list -> unit
-  = "caml_Mix_Init"
-external quit : unit -> unit
-  = "caml_Mix_Quit"
-external open_audio :
-     frequency:int
+external init : InitFlags.t list -> unit = "caml_Mix_Init"
+external quit : unit -> unit = "caml_Mix_Quit"
+
+external open_audio
+  :  frequency:int
   -> audio_format:Sdl.AudioFormat.t
   -> channels:int
-  -> chunk_size:int -> unit = "caml_Mix_OpenAudio"
-external close_audio : unit -> unit
-  = "caml_Mix_CloseAudio"
-external get_error : unit -> string
-  = "caml_Mix_GetError"
+  -> chunk_size:int
+  -> unit
+  = "caml_Mix_OpenAudio"
+
+external close_audio : unit -> unit = "caml_Mix_CloseAudio"
+external get_error : unit -> string = "caml_Mix_GetError"
 
 (* samples *)
 
@@ -37,28 +37,25 @@ module Chunk = struct
   type t
 end
 
-external load_wav : string -> Chunk.t
-  = "caml_Mix_LoadWAV"
-external free_chunk : Chunk.t -> unit
-  = "caml_Mix_FreeChunk"
+external load_wav : string -> Chunk.t = "caml_Mix_LoadWAV"
+external free_chunk : Chunk.t -> unit = "caml_Mix_FreeChunk"
 
 (* channels *)
 
-external play_channel :
-     channel:int
+external play_channel
+  :  channel:int
   -> chunk:Chunk.t
   -> loops:int
-  -> unit = "caml_Mix_PlayChannel"
+  -> unit
+  = "caml_Mix_PlayChannel"
 
 (* music *)
 
-module Music = struct type t end
+module Music = struct
+  type t
+end
 
-external load_mus : string -> Music.t
-  = "caml_Mix_LoadMUS"
-external free_music : Music.t -> unit
-  = "caml_Mix_FreeMusic"
-external play_music : Music.t -> loop:int -> unit
-  = "caml_Mix_PlayMusic"
-external fade_out_music : ms:int -> unit
-  = "caml_Mix_FadeOutMusic"
+external load_mus : string -> Music.t = "caml_Mix_LoadMUS"
+external free_music : Music.t -> unit = "caml_Mix_FreeMusic"
+external play_music : Music.t -> loop:int -> unit = "caml_Mix_PlayMusic"
+external fade_out_music : ms:int -> unit = "caml_Mix_FadeOutMusic"
--- mix.mli
+++ mix.mli
@@ -1,4 +1,6 @@
-(** Ocaml binding to libsdl2 mixer. See {:https://www.libsdl.org/projects/old/SDL_mixer/docs/SDL_mixer_frame.html} for the documentation *)
+(** Ocaml binding to libsdl2 mixer.  See
+    {:https://www.libsdl.org/projects/old/SDL_mixer/docs/SDL_mixer_frame.html}
+    for the documentation *)
 
 (** {1:gen General} *)
 
@@ -9,6 +11,7 @@ module InitFlags : sig
     | `MIX_INIT_MP3
     | `MIX_INIT_OGG
     ]
+
   val test : unit -> unit
 end
 
@@ -20,65 +23,63 @@ module Default : sig
   val chunk_size : int
 end
 
-external init : InitFlags.t list -> unit
-  = "caml_Mix_Init"
-external quit : unit -> unit
-  = "caml_Mix_Quit"
-external open_audio :
-     frequency:int
+external init : InitFlags.t list -> unit = "caml_Mix_Init"
+external quit : unit -> unit = "caml_Mix_Quit"
+
+external open_audio
+  :  frequency:int
   -> audio_format:Sdl.AudioFormat.t
   -> channels:int
-  -> chunk_size:int -> unit = "caml_Mix_OpenAudio"
-external close_audio : unit -> unit
-  = "caml_Mix_CloseAudio"
-external get_error : unit -> string
-  = "caml_Mix_GetError"
+  -> chunk_size:int
+  -> unit
+  = "caml_Mix_OpenAudio"
+
+external close_audio : unit -> unit = "caml_Mix_CloseAudio"
+external get_error : unit -> string = "caml_Mix_GetError"
 
 (** {1:samples Samples} *)
 
-module Chunk : sig type t end
+module Chunk : sig
+  type t
+end
 
 (** {2:samples_loading Loading} *)
 
-external load_wav : string -> Chunk.t
-  = "caml_Mix_LoadWAV"
+external load_wav : string -> Chunk.t = "caml_Mix_LoadWAV"
 
 (** {2:samples_freeing Freeing} *)
 
-external free_chunk : Chunk.t -> unit
-  = "caml_Mix_FreeChunk"
+external free_chunk : Chunk.t -> unit = "caml_Mix_FreeChunk"
 
 (** {1:ch Channels} *)
 
 (** {2:ch_playing Playing} *)
 
-external play_channel :
-     channel:int
+external play_channel
+  :  channel:int
   -> chunk:Chunk.t
   -> loops:int
-  -> unit = "caml_Mix_PlayChannel"
+  -> unit
+  = "caml_Mix_PlayChannel"
 
 (** {1:mus Music} *)
 
-module Music : sig type t end
+module Music : sig
+  type t
+end
 
 (** {2:mus_load Loading} *)
 
-external load_mus : string -> Music.t
-  = "caml_Mix_LoadMUS"
+external load_mus : string -> Music.t = "caml_Mix_LoadMUS"
 
 (** {2:mus_free Free} *)
 
-external free_music : Music.t -> unit
-  = "caml_Mix_FreeMusic"
+external free_music : Music.t -> unit = "caml_Mix_FreeMusic"
 
 (** {2:mus_play Playing} *)
 
-external play_music : Music.t -> loop:int -> unit
-  = "caml_Mix_PlayMusic"
+external play_music : Music.t -> loop:int -> unit = "caml_Mix_PlayMusic"
 
 (** {2:mus_play Stopping} *)
 
-external fade_out_music : ms:int -> unit
-  = "caml_Mix_FadeOutMusic"
-
+external fade_out_music : ms:int -> unit = "caml_Mix_FadeOutMusic"
