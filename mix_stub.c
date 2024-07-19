0fda207d0bce1bf1b34cef7d735f818fb6af6e30
seb <seb@seb.seb>
2024-07-19 13:17:41 +0200

    playing fix

--- mix_stub.c
+++ mix_stub.c
@@ -192,7 +192,7 @@ caml_Mix_PlayingMusic(value unit)
 {
     CAMLparam1(unit);
     int ret = Mix_PlayingMusic();
-    CAMLreturn(Bool_val(ret));
+    CAMLreturn(ret = 1 ? Val_true : Val_false);
 }
 
 
